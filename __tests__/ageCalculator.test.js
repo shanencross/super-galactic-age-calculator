6856cb734911ff0ac016e7069eab2494d644ccd8
Shanen Cross <shanencross@yahoo.com>
2021-03-14 03:36:03 -0700

    Rename age property to earthAge

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -3,6 +3,6 @@ import AgeCalculator from "./../src/ageCalculator.js"
 describe('AgeCalculator', () => {
 	test('Should create an age calculator object with the proper age', () => {
 		let ageCalculator = new AgeCalculator(22);
-		expect(ageCalculator.age).toEqual(22);
+		expect(ageCalculator.earthAge).toEqual(22);
 	});
 });
