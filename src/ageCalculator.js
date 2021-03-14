f3926b8e2a97602a1d03e64c1e5f6b71748343b2
Shanen Cross <shanencross@yahoo.com>
2021-03-14 04:16:21 -0700

    Pass tests 2-5

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -1,5 +1,36 @@
 export default class AgeCalculator {
 	constructor(earthAge) {
+		this.planetYearsPerEarthYear = {
+			"Mercury": 0.24,
+			"Venus": 0.62,
+			"Mars": 1.88,
+			"Jupiter": 11.86
+		}
+
 		this.earthAge = earthAge;
+		this.mercuryAge = this.calculateMercuryAge();
+		this.venusAge = this.caluclateVenusAge();
+		this.marsAge = this.calculateMarsAge();
+		this.jupiterAge = this.calculateJupiterAge();
+	}
+
+	calculatePlanetAge(planetName) {
+		return Math.floor(this.earthAge / this.planetYearsPerEarthYear[planetName]);
+	}
+
+	calculateMercuryAge() {
+		return this.calculatePlanetAge("Mercury");
+	}
+
+	caluclateVenusAge() {
+		return this.calculatePlanetAge("Venus");
+	}
+
+	calculateMarsAge() {
+		return this.calculatePlanetAge("Mars");
+	}
+
+	calculateJupiterAge() {
+		return this.calculatePlanetAge("Jupiter");
 	}
 }
